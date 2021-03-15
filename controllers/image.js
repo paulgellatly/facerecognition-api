90d822a02b0cbe8d0301fcd2fc6638491f74a81d
Paul G <paulgellatly@icloud.com>
2021-03-15 18:49:02 +0000

    security review

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,23 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '04b8274fae9d43ce920b9486416a8327',
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .initModel({
+            id: Clarifai.FACE_DETECT_MODEL,
+        })
+        .then((faceDetectModel) => {
+            return faceDetectModel.predict(req.body.input);
+        })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users')
@@ -12,4 +32,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
     handleImage,
+    handleApiCall,
 };
