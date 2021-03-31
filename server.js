bb2d68c433ac2a2c1c406b5f39420ef1ba04d6ab
Paul G <paulgellatly@icloud.com>
2021-03-31 18:28:59 +0100

    more database addons

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-silhouetted-36444',
-        user: 'paulgellatly',
-        password: '',
-        database: 'smart-brain',
+        host: process.env.DATABASE_URL,
+        ssl: true,
     },
 });
 
